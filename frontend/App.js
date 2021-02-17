b3cb17ed9f917e53d081f076a905ef861631737b
Ken Johnson <dragonkenkj@gmail.com>
2021-02-17 13:23:19 -0500

    Organized src

--- frontend/App.js
+++ frontend/App.js
@@ -1,12 +1,11 @@
 import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { StyleSheet, SafeAreaView } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-// import { navigationRef } from "./RootNavigation";
+import { Colors } from "./src/styles";
 
-import NavBar from "./NavBar";
+import NavBar from "./src/navigations/NavBar";
 
 // const Stack = createStackNavigator();
 
@@ -20,11 +19,9 @@ export default function App() {
   );
 }
 
-const colors = { primary: "#05386B", secondary: "#5CDB95" };
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: colors.secondary,
+    backgroundColor: Colors.SECONDARY,
   },
 });
