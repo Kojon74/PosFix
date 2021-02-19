7ce04eedfeb0e5c5fb0f8bc655aab497f6171208
dpbaines <david.peter.baines@gmail.com>
2021-02-19 16:06:42 -0500

    Adding new function to imu_calc.py to support a time vector array which should give more accurate results, still trying to figure out why orientation isn't conserved...

--- rpi/getpos.py
+++ rpi/getpos.py
@@ -8,7 +8,7 @@ from skinematics import quat
 
 import numpy as np
 
-import time
+import time as time_lib
 
 initial_orientation = np.array([[1.,0.,0.],[0.,1.,0.],[0.,0.,1.]])
 
@@ -20,65 +20,92 @@ initial_orientation = np.array([[1.,0.,0.],[0.,1.,0.],[0.,0.,1.]])
 
 bmx = BMX160(1)
 
-NUM_SAMPLES = 10
-DEBUG_MODE = 1
+NUM_SAMPLES = 100
+DEBUG_MODE = 0
 
+# Get the IMU data and return:
+'''
+accel = ndarray(NUM_SAMPLES,3) = Acceleration vector
+gyro  = ndarray(NUM_SAMPLES,3) = Angulary velocity vector
+magn  = ndarray(NUM_SAMPLES,3) = Magnetic field strength vector
+rate  = float                  = Average rate
+time  = ndarray(NUM_SAMPLES,1) = Time axis for integration, due to uneven sampling rate
+'''
 def get_imu_data():
     accel = np.zeros(shape=(NUM_SAMPLES,3))
     gyro = np.zeros(shape=(NUM_SAMPLES,3))
     magn = np.zeros(shape=(NUM_SAMPLES,3))
 
+    time_vec = np.zeros(NUM_SAMPLES)
+
     cumtime = 0
 
     for i in range(NUM_SAMPLES):
-        currtime = time.time() * 1000; 
+        currtime = time_lib.time()
         data = bmx.get_all_data()
-        cumtime += (time.time() * 1000) - currtime
+        finishtime = (time_lib.time()) - currtime
+        cumtime += finishtime
 
         accel[i] = [data[6], data[7], data[8]]
         gyro[i] = [data[3], data[4], data[5]]
         magn[i] = [data[0], data[1], data[2]]
+        
+        time_vec[i] = finishtime
     
     samp_period = cumtime / NUM_SAMPLES
 
-    return accel, gyro, magn, samp_period
+    return accel, gyro, magn, samp_period, time_vec
 
 # position_computer = imus.Mahony(Sample.per#)
 
-# orientation quaternions
-q = np.zeros(shape=(NUM_SAMPLES,4)) # [quat.Quaternion([0., 0., 0., 0.]) for i in range(120)]
+def main_loop():
+    # orientation quaternions
+    q = np.zeros(shape=(NUM_SAMPLES,4)) # [quat.Quaternion([0., 0., 0., 0.]) for i in range(120)]
 
-# position
-pos = np.zeros(shape=(NUM_SAMPLES,3))
+    # position
+    pos = np.zeros(shape=(NUM_SAMPLES,3))
 
-# velocity
-vel = np.zeros(shape=(NUM_SAMPLES,3))
+    # velocity
+    vel = np.zeros(shape=(NUM_SAMPLES,3))
 
-# Setup initial orientation, velocity and position
+    # Setup initial orientation, velocity and position
 
-q[NUM_SAMPLES - 1] = np.array([1., 0., 0., 0.]) # quat.Quaternion(np.array([1.,0.,0.,0.]))
+    q[NUM_SAMPLES - 1] = np.array([1., 0., 0., 0.]) # quat.Quaternion(np.array([1.,0.,0.,0.]))
 
-while 1:
+    # Wait for IMU  to init
 
-    # Read IMU data and place into matrices to be analyzed in batches
-    accel, gyro, magn, samp_period = get_imu_data()
+    while not bmx.begin():
+        print("Waiting for IMU...")
+        time_lib.sleep(1)
 
-    samp_freq = 1/(samp_period * (10**-3))
+    while 1:
 
-    # print(q[119])
+        # Read IMU data and place into matrices to be analyzed in batches
+        accel, gyro, magn, samp_period, time = get_imu_data()
 
-    # Convert initial rotation quaternion into rotation matrix
-    initial_rot = quat.convert(q[NUM_SAMPLES - 1], to='rotmat') # .export(to='rotmat')
+        samp_freq = 1/(samp_period)
 
-    # Use analytical method to calculate position, orientation and velocity
-    (q, pos, vel) = imu_calc.analytical(omega = gyro, 
-                                        accMeasured = accel, 
-                                        rate = samp_freq, 
-                                        R_initialOrientation = initial_rot, 
-                                        initialPosition = pos[NUM_SAMPLES - 1])
+        # print(q[119])
 
-    if DEBUG_MODE == 1:
-        print(gyro)
-        print(samp_freq)
-    
-    print("Latest Position: {:.2f} {:.2f} {:.2f}, Orientation: {:.2f} {:.2f} {:.2f} {:.2f}".format(pos[NUM_SAMPLES - 1][0], pos[NUM_SAMPLES - 1][1], pos[NUM_SAMPLES - 1][2], q[NUM_SAMPLES - 1][0], q[NUM_SAMPLES - 1][1], q[NUM_SAMPLES - 1][2], q[NUM_SAMPLES - 1][3]))
+        # Convert initial rotation quaternion into rotation matrix
+        initial_rot = quat.convert(q[NUM_SAMPLES - 1], to='rotmat') # .export(to='rotmat')
+
+        # Use analytical method to calculate position, orientation and velocity
+        (q, pos, vel) = imu_calc.calc_orientation_position(omega = gyro, 
+                                            accMeasured = accel, 
+                                            rate = samp_freq, 
+                                            initialOrientation = initial_rot, 
+                                            initialPosition = pos[NUM_SAMPLES - 1],
+                                            timeVector=time)
+
+        if DEBUG_MODE == 1:
+            print("Gyro")
+            print(gyro)
+            print("Accel")
+            print(accel)
+            print(samp_freq)
+        
+        print("Latest Position: {:.2f} {:.2f} {:.2f}, V: {:.2f} {:.2f} {:.2f},  Orientation: {:.2f} {:.2f} {:.2f} {:.2f}".format(pos[NUM_SAMPLES - 1][0], pos[NUM_SAMPLES - 1][1], pos[NUM_SAMPLES - 1][2], vel[NUM_SAMPLES - 1][0], vel[NUM_SAMPLES - 1][1], vel[NUM_SAMPLES - 1][2], q[NUM_SAMPLES - 1][0], q[NUM_SAMPLES - 1][1], q[NUM_SAMPLES - 1][2], q[NUM_SAMPLES - 1][3]))
+
+
+main_loop()
--- rpi/imu_calc.py
+++ rpi/imu_calc.py
@@ -103,3 +103,85 @@ def analytical(R_initialOrientation=np.eye(3),
         pos[:,ii] = cumtrapz(vel[:,ii], dx=1./rate, initial=initialPosition[ii])
 
     return (q, pos, vel)
+
+# Similar to above analytical solver but with changes for real time use, and varying dx for the integration steps
+# This is modified from the scipy-kinematics library function
+def calc_orientation_position(initialOrientation=np.eye(3),
+                                omega=np.zeros((5,3)),
+                                initialPosition=np.zeros(3),
+                                initialVelocity=np.zeros(3),
+                                timeVector=np.zeros((5,1)),
+                                accMeasured=np.column_stack((np.zeros((5,2)), 9.81*np.ones(5))),
+                                rate=100):
+    ''' Reconstruct position and orientation with an analytical solution,
+    from angular velocity and linear acceleration.
+    Assumes a start in a stationary position. No compensation for drift.
+
+    Parameters
+    ----------
+    R_initialOrientation: ndarray(3,3)
+        Rotation matrix describing the initial orientation of the sensor,
+        except a mis-orienation with respect to gravity
+    omega : ndarray(N,3)
+        Angular velocity, in [rad/s]
+    initialPosition : ndarray(3,)
+        initial Position, in [m]
+    accMeasured : ndarray(N,3)
+        Linear acceleration, in [m/s^2]
+    rate : float
+        sampling rate, in [Hz]
+
+    Returns
+    -------
+    q : ndarray(N,3)
+        Orientation, expressed as a quaternion vector
+    pos : ndarray(N,3)
+        Position in space [m]
+    vel : ndarray(N,3)
+        Velocity in space [m/s]
+    '''
+
+    if omega.ndim == 1:
+        raise ValueError('The input to "analytical" requires matrix inputs.')
+        
+    # Transform recordings to angVel/acceleration in space --------------
+
+    # Orientation of \vec{g} with the sensor in the "R_initialOrientation"
+    g = constants.g
+    g0 = np.linalg.inv(initialOrientation).dot(np.r_[0,0,g])
+
+    # for the remaining deviation, assume the shortest rotation to there
+    q0 = vector.q_shortest_rotation(accMeasured[0], g0)    
+    
+    q_initial = rotmat.convert(initialOrientation, to='quat')
+    
+    # combine the two, to form a reference orientation. Note that the sequence
+    # is very important!
+    q_ref = quat.q_mult(q_initial, q0)
+    
+    # Calculate orientation q by "integrating" omega -----------------
+    q = quat.calc_quat(omega, q_ref, rate, 'bf')
+
+    # Acceleration, velocity, and position ----------------------------
+    # From q and the measured acceleration, get the \frac{d^2x}{dt^2}
+    g_v = np.r_[0, 0, g] 
+    accReSensor = accMeasured - vector.rotate_vector(g_v, quat.q_inv(q))
+    accReSpace = vector.rotate_vector(accReSensor, q)
+
+    # Make the first position the reference position
+    q = quat.q_mult(q, quat.q_inv(q[0]))
+
+    # compensate for drift
+    #drift = np.mean(accReSpace, 0)
+    #accReSpace -= drift*0.7
+
+    # Position and Velocity through integration, assuming 0-velocity at t=0
+    vel = np.nan*np.ones_like(accReSpace)
+    pos = np.nan*np.ones_like(accReSpace)
+
+    for ii in range(accReSpace.shape[1]):
+        vel[:,ii] = cumtrapz(accReSpace[:,ii], x=timeVector, initial=initialVelocity[ii])
+        pos[:,ii] = cumtrapz(vel[:,ii], x=timeVector, initial=initialPosition[ii])
+
+    return (q, pos, vel)
+    
