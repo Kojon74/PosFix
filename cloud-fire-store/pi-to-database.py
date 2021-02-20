15e62a808be07af48d4e59d208690e9a0833a555
Akinori Kimura <1999akinori@gmail.com>
2021-02-20 13:07:38 -0500

    heuristics

--- cloud-fire-store/pi-to-database.py
+++ cloud-fire-store/pi-to-database.py
@@ -6,6 +6,7 @@ from orientation import read_orientation
 from flexsensor import read_flex
 from firebase_admin import credentials
 from firebase_admin import firestore
+from quaternion import quaternion_to_euler
 
 # Use a service account
 cred = credentials.Certificate("/home/pi/mu_code/makeuoft2021/posfix/posfix-efa73-firebase-adminsdk-6d6mq-ca65a76d9e.json")
@@ -24,6 +25,8 @@ def on_snapshot(doc_snapshot, changes, read_time):
         if doc.get('isStarted'):
             session = u'session_1'
         else:
+            if session != u'posture-data':
+                print('meh')
             session = u'posture-data'
     callback_done.set()
 
@@ -37,27 +40,51 @@ i=1
 
 # default document
 session = u'posture-data'
+num_session = 0
 
+# Total value for each parameter to calculate average
+total_back = 0
+total_orientation = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
 
 while True:
     # flex sensor
     back_curviture = read_flex()
+    if session != u'posture-data':
+        total_back += back_curviture
     print(back_curviture)
     
     # imu sensor
     imu_sensors = read_orientation()
+    orientation_0 = quaternion_to_euler(imu_sensors[0][0], imu_sensors[0][1], imu_sensors[0][2], imu_sensors[0][3])
+    orientation_1 = quaternion_to_euler(imu_sensors[1][0], imu_sensors[1][1], imu_sensors[1][2], imu_sensors[1][3])
+    orientation_2 = quaternion_to_euler(imu_sensors[2][0], imu_sensors[2][1], imu_sensors[2][2], imu_sensors[2][3])
+    orientation_3 = quaternion_to_euler(imu_sensors[3][0], imu_sensors[3][1], imu_sensors[3][2], imu_sensors[3][3])
+    orientation_matrix = [orientation_0, orientation_1, orientation_2, orientation_3]
+    
+    # session total for imu_sensor
+    if session != u'posture-data':
+        for i in range(4):
+           # iterate through columns
+           for j in range(4):
+               total_orientation[i][j] = total_orientation[i][j] + orientation_matrix[i][j]
+    
     
     # Warning from back sensor
     if back_curviture < 18:
         data = {
             u'message': "your leaning too far back",
         }
-        db.collection(u'notification').document(u'notification_2').set(data)
-    if back_curviture < 26:
+        db.collection(u'notification').document(u'notification').set(data)
+    elif back_curviture > 26:
         data = {
             u'message': "your leaning too far in buddy",
         }
-        db.collection(u'notification').document(u'notification_2').set(data)
+        db.collection(u'notification').document(u'notification').set(data)
+    else:
+        data = {
+            u'message': "",
+        }
+        db.collection(u'notification').document(u'notification').set(data)
     
     data = {
         u'time': datetime.datetime.now(),
@@ -75,4 +102,8 @@ while True:
             u'orientation': imu_sensors[3]
         },
     }
+    
+    # increment the number of data points in the session
+    num_session += 1
+    
     db.collection(u'test').document(str(datetime.datetime.now())).set(data)
