3b7166396b51b27d0452a43f6800ab0d87daca4f
Akinori Kimura <1999akinori@gmail.com>
2021-02-20 01:43:43 -0500

    notification and start functionality

--- cloud-fire-store/pi-to-database.py
+++ cloud-fire-store/pi-to-database.py
@@ -1,21 +1,55 @@
 # get firebase_admin by `sudo pip install firebase_admin`
 import firebase_admin
 import datetime
+import threading
 from flexsensor import read_flex
 from firebase_admin import credentials
 from firebase_admin import firestore
 
 # Use a service account
-cred = credentials.Certificate("/home/pi/mu_code/makeuoft2021/posfix/PosFix/cloud-fire-store/posfix-efa73-firebase-adminsdk-6d6mq-dc145efbb0.json")
+cred = credentials.Certificate("/home/pi/mu_code/makeuoft2021/posfix/posfix-efa73-firebase-adminsdk-6d6mq-ca65a76d9e.json")
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
+# Create an Event for notifying main thread.
+callback_done = threading.Event()
 
+# Create a callback on_snapshot function to capture changes
+def on_snapshot(doc_snapshot, changes, read_time):
+    for doc in doc_snapshot:
+        print(f'Received document snapshot: {doc.id}')
+        global session
+        if doc.get('isStarted'):
+            session = u'session_1'
+        else:
+            session = u'posture-data'
+    callback_done.set()
 
-for x in range(1,10000):
+doc_ref = db.collection(u'variables').document(u'start')
+
+# Watch the document
+doc_watch = doc_ref.on_snapshot(on_snapshot)
+
+i=1
+session = u'posture-data'
+
+while True:
     back_curviture = read_flex()
-    print(x)
+    print(back_curviture)
+    
+    # Warning from back sensor
+    if back_curviture < 18:
+        data = {
+            u'message': "your leaning too far back",
+        }
+        db.collection(u'notification').document(u'notification_2').set(data)
+    if back_curviture < 26:
+        data = {
+            u'message': "your leaning too far in buddy",
+        }
+        db.collection(u'notification').document(u'notification_2').set(data)
+    
     data = {
         u'time': datetime.datetime.now(),
         u'back-curviture': back_curviture,
@@ -35,4 +69,5 @@ for x in range(1,10000):
             u'orientation': [1,1,1,1]
         },
     }
-    db.collection(u'posture-data').document(str(x)).set(data)
+    print(session)
+    db.collection(session).document(str(datetime.datetime.now())).set(data)
